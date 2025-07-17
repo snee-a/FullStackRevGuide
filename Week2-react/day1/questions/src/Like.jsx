8377cd22626664d60bf0316e7d0f956f1ac88254
Sneha Pandey <165113180+snee-a@users.noreply.github.com>
2025-07-17 11:34:15 +0530

    Delete Week2-react/day1/questions directory

--- Week2-react/day1/questions/src/Like.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-export default function Like(){
-    const [isliked, setisliked] = useState(false);
-    return (
-        <p onClick={()=>{
-            setisliked(!isliked)
-        }}><i className={isliked ? "fa-solid fa-heart" : "fa-regular fa-heart"}></i></p>
-    )
-}
